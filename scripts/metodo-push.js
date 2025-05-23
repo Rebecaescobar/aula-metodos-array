c120c6fe9887e95f0eae205e750ed860349cc8e7
Rebequinha <rebeca.escobar@aluno.senai.br>
2025-05-23 08:59:55 -0300

    feat: usa o método push e edita o arquivo README.md

---
+++ scripts/metodo-push.js
@@ -0,0 +1,9 @@
+let playlist = ["Funk", "Hip-Hop", "Pop"];
+
+console.log("Antes do push");
+console.table(playlist);
+
+playlist.push("Trap");
+
+console.log("Depois do push");
+console.table(playlist);
